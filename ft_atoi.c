928a6e66e4c279c11e243e54e631416f29ca7625
Abdelaziz El Abdi <ael-abdi@e2r10p2.1337.ma>
2024-04-25 11:25:00 +0100

    problem

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: ael-abdi <ael-abdi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 15:57:16 by ael-abdi          #+#    #+#             */
-/*   Updated: 2023/11/27 15:38:05 by ael-abdi         ###   ########.fr       */
+/*   Updated: 2024/04/25 11:24:37 by ael-abdi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,29 @@
 
 int	ft_atoi(const char *str)
 {
-	int		l;
+	int		d;
 	int		mr;
 	size_t	ben;
 
-	l = 0;
+	d = 0;
 	mr = 1;
 	ben = 0;
-	while (str[l] == 32 || (str[l] >= 9 && str[l] <= 13))
-		l++;
-	if (str[l] == '-' || str[l] == '+')
+	while (str[d] == 32 || (str[d] >= 9 && str[d] <= 13))
+		d++;
+	if (str[d] == '-' || str[d] == '+')
 	{
-		if (str[l] == '-')
+		if (str[d] == '-')
 			mr = -mr;
-		l++;
+		d++;
 	}
-	while (str[l] >= '0' && str[l] <= '9')
+	while (str[d] >= '0' && str[d] <= '9')
 	{
-		ben = ben * 10 + str[l] - 48;
+		ben = ben * 10 + str[d] - 48;
 		if (ben >= 9223372036854775807 && mr == 1)
 			return (-1);
 		else if (ben > 9223372036854775807 && mr == -1)
 			return (0);
-		l++;
+		d++;
 	}
 	return (mr * ben);
 }
